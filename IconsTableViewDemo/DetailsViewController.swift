10a19f9599414154fe9ca3cf5cb94d90227e81ce
tomerbuzaglo <tomer.bu@gmail.com>
2016-04-17 14:20:51 +0300

    added detail view controller

--- IconsTableViewDemo/DetailsViewController.swift
+++ IconsTableViewDemo/DetailsViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    @IBOutlet weak var labelTopConstraint: NSLayoutConstraint!
+    
+    @IBAction func tapped(sender: UITapGestureRecognizer) {
+        self.labelTopConstraint.constant += 20
+        UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: UIViewAnimationOptions.BeginFromCurrentState, animations: {
+            self.view.layoutIfNeeded()
+        }) { (_) in
+            self.labelTopConstraint.constant -= 20
+            self.view.layoutIfNeeded()
+            UIView.animateWithDuration(0.4, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+    }
+    var data:Icon?{
+        didSet{
+            initLayout()
+        }
+    }
+    
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initLayout()
     }
-
+    
+    func initLayout(){
+        subtitleLabel?.text = data?.title
+        titleLabel?.text = data?.title
+        imageView?.image = data?.image
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
--- IconsTableViewDemo/IconsTableViewController.swift
+++ IconsTableViewDemo/IconsTableViewController.swift
@@ -133,14 +133,20 @@ class IconsTableViewController: UITableViewController {
         return [action]
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    
+    // MARK: - Navigation
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let icon = dataSource.getItemAtIndexPath(indexPath)
+        performSegueWithIdentifier("MasterToDetail", sender: icon)
+    }
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if let dest = segue.destinationViewController as? DetailsViewController{
+            dest.data = (sender as! Icon)
+        }
+    }
     
 }
